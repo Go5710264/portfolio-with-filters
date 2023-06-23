94b5c6ef7aa5678452ceee66ded16152f8b1d9f0
Dima Shirkov <dimashirkov@list.ru>
2023-06-23 18:51:47 +0300

    fix link vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://go5710264.github.io/portfolio-with-filters//"
+  base: "/portfolio-with-filters/"
 })
